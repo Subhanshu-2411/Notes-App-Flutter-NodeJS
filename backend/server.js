a3a7d1bfecfaed450b02defd9c7b4f9ff73ce733
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-05-13 03:34:42 +0530

    Added MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,23 @@ const express = require('express');
 
 const app = express();
 
-// Home Page
-app.get("/", function(req, res) {
-    res.send("Home Page");
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://subhanshu:ramayana1@cluster0.bm8slnx.mongodb.net/?retryWrites=true&w=majority").then(function() {
+    // Home Page
+    app.get("/", function(req, res) {
+        res.send("Home Page");
+    });
+
+    // Notes Page
+    app.get("/notes", function(req, res) {
+        res.send("Notes Page");
+    });
 });
 
-// Notes Page
-app.get("/notes", function(req, res) {
-    res.send("Notes Page");
-});
+
+
+
 
 app.listen(5000, function() {
     console.log(`Server started at Port 5000`);
