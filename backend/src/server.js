59c473a2aae32c171b1f2461dd490a2cb604f189
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-05-16 01:11:23 +0530

    Added Hosting Additions

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,9 @@ app.use(
     bodyParser.json()
 )
 
-mongoose.connect("mongodb+srv://subhanshu:ramayana1@cluster0.bm8slnx.mongodb.net/?retryWrites=true&w=majority").then(function() {
+const DBPath = "mongodb+srv://subhanshu:ramayana1@cluster0.bm8slnx.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DBPath).then(function() {
     // Home Page
     app.get("/", function(req, res) {
         res.json({
@@ -35,7 +37,7 @@ mongoose.connect("mongodb+srv://subhanshu:ramayana1@cluster0.bm8slnx.mongodb.net
 
 
 
-
-app.listen(5000, function() {
-    console.log(`Server started at Port 5000`);
+const PORT = process.env.PORT || 5000
+app.listen(PORT, function() {
+    console.log(`Server started at Port ${PORT}` );
 });
